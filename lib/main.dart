c353ce0ab5e1dd0888f1a334b3db461830484f7d
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2023-08-16 02:08:00 -0300

    feat: criar helper para ler variáveis de ambiente

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'src/barbershop_app.dart';
+import 'src/core/helpers/env_helper.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await EnvHelper.instance.load();
+
   runApp(
     const ProviderScope(
       child: BarbershopApp(),
