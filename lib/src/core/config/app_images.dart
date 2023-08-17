cfa9671cb606466c89df18adea02faee84ac3b92
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2023-08-17 12:55:29 -0300

    feat: criar classe AppImages para melhor acesso às imagens dos assets

---
+++ lib/src/core/config/app_images.dart
@@ -0,0 +1,5 @@
+class AppImages {
+  static const backgroundChair = 'assets/images/background_image_chair.jpg';
+  static const imageLogo = 'assets/images/imgLogo.png';
+  static const avatar = 'assets/images/avatar.png';
+}
